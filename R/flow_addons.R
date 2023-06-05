e745273c7e92f445b9156f6bdab00f0d5c2b7753
Erickson <12001003029097@FEDIDCARD.GOV>
2023-06-05 13:28:26 -0600

    added tests and transmute() to get_rbi()

--- R/flow_addons.R
+++ R/flow_addons.R
@@ -4,18 +4,22 @@
 #'
 #' @param data A daily value data.frame.
 #' @param flow_var A column name for flow, e.g. Flow.
-#' @param ... values to pass to `dplyr::group_by()` to perform RBI calculation on.
+#' @param ... values to pass to `dplyr::group_by()` to perform RBI calculation on. (optional)
 #'
 #' @return A rbi column within data.frame.
-#' @note The data.frame must ha
+#' @note The user must specify the `flow_var` column but it's optional whether to use `dplyr::group_by()` or not.
+#' @export
 #'
+#' @examples
+#' stream_flow <- data.frame(flow = c(seq(30, 60), seq(60, 30, length.out = 60)))
+#' stream_flow %>% get_rbi(flow)
 
 get_rbi <- function(data, flow_var, ...) {
 
     data %>%
         dplyr::mutate(lagged_flow = dplyr::lag({{flow_var}})) %>%
         dplyr::group_by(...) %>%
-        dplyr::mutate(rbi = sum(abs({{flow_var}}-lagged_flow), na.rm = T)/sum({{flow_var}}, na.rm = T)) %>%
+        dplyr::transmute(rbi = sum(abs({{flow_var}}-lagged_flow), na.rm = T)/sum({{flow_var}}, na.rm = T)) %>%
         dplyr::slice(1) %>%
         dplyr::ungroup()
 
---
+++ tests/testthat/test-flow_addons.R
@@ -0,0 +1,27 @@
+test_that("testing the flow_addons.R scripts", {
+
+    #### Testing get_rbi()
+
+    stream_flow <- data.frame(flow = c(seq(30, 60), seq(60, 30, length.out = 60)))
+    rbi <- stream_flow %>% get_rbi(flow)
+
+    testthat::expect_equal(round(rbi[,3], 3)$rbi, 0.015)
+
+    # now with two stations and different flows
+    stream_flow <- data.frame(flow = c(seq(30, 60), seq(60, 30, length.out = 60),
+                                       seq(30, 600, length.out = 2),
+                                       seq(600, 40, length.out = 7),
+                                       seq(40, 800, length.out = 2),
+                                       seq(800, 20, length.out = 8),
+                                       seq(20, 800, length.out = 2),
+                                       seq(800, 10, length.out = 8),
+                                       seq(10, 1200, length.out = 62)),
+                              station = c(rep('snowmelt', 91), rep('rain', 91)))
+
+    rbi <- stream_flow %>% get_rbi(flow, station)
+
+    testthat::expect_equal(round(rbi[,4], 3)$rbi, c(0.112, 0.015))
+
+
+
+})
